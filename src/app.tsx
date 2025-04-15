3cee485ee5104ed45e629d031bb8a2916e33f991
sashayio <qborodinov@gmail.com>
2025-04-15 13:57:08 +0300

    150425

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import './app.css'
 import TimeSinceTimestamp from './timestamp'
 
-const timestamp = 1744480800;
+const timestamp = 1744714500;
 
 export function App() {
   return (
     <>
+      <h1 style={{color: 'gold'}}>Пожалуйста</h1>
       <h1>Timestamp {timestamp}</h1>
       <TimeSinceTimestamp timestamp={timestamp} />
     </>
