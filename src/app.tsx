163b0cd108dc82a58eb706a5e366206318be223e
sashayio <qborodinov@gmail.com>
2026-01-26 14:47:35 +0300

    take

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import './app.css'
 import TimeSinceTimestamp from './timestamp'
 
-const timestamp = 1768867200;
+const timestamp = 1769472000;
 
 export function App() {
   return (
     <>
       <h1>Timestamp {timestamp}</h1>
-      <h2>first commit: 1 year ago (June 7, 2024 at 11:23 AM)</h2>
+      <h2>first commit: 1 year, 7 m, 21d, ago (June 7, 2024 at 11:23 AM)</h2>
       <h2>think about it</h2>
       <TimeSinceTimestamp timestamp={timestamp} />
     </>
